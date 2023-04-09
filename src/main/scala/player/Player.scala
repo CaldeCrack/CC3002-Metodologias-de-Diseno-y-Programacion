2e5dc42629734300f21da3e67840c228b2681d06
Andrés Calderón <an.caldeguardia@gmail.com>
2023-04-09 02:01:05 -0400

    Added player class and fixed tests

---
+++ src/main/scala/player/Player.scala
@@ -0,0 +1,13 @@
+package cl.uchile.dcc
+package player
+
+import card.Card
+class Player(val name: String, var gems: Int = 2, var hand: Array[Card] = new Array[Card](10), var deck: Array[Card]) {
+  def loseGems(less: Int): Unit = {
+
+  }
+
+  def drawCard(): Unit = {
+
+  }
+}
--- src/test/scala/playerTests/PlayerTests.scala
+++ src/test/scala/playerTests/PlayerTests.scala
@@ -1,13 +1,15 @@
 package cl.uchile.dcc
 package playerTests
 
+import cl.uchile.dcc.card.Card
+import cl.uchile.dcc.player.Player
 import munit.FunSuite
 class PlayerTests extends FunSuite {
   val name = "Player"
 
   var player: Player = _
   override def beforeEach(context: BeforeEach): Unit = {
-    player = new Player(name)
+    player = new Player(name, deck = new Array[Card](25))
   }
   test("A Player has a name") {
     assertEquals(player.name, name)
@@ -17,15 +19,15 @@ class PlayerTests extends FunSuite {
     assertEquals(player.gems, 2)
   }
 
-  test("A Player can lose its gems") {
+  test("A Player can lose gems") {
     player.loseGems(1)
     assertEquals(player.gems, 1)
     player.loseGems(1)
     assertEquals(player.gems, 0)
   }
 
-  test("A Player initially has a hand (of 0 cards)") {
-    assertEquals(player.hand, Array[Card]())
+  test("A Player initially has a hand (of max 10 cards)") {
+    assertEquals(player.hand.length, 10)
   }
 
   test("A Player can draw cards") {
